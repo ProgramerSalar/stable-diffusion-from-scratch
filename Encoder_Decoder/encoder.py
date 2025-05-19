091a5f35da479dd30049af6cfe77e3d8244223e9
manish kumar <manishkumar60708090@gmail.com>
2025-05-19 16:09:22 +0530

    test the Encoder

--- Encoder_Decoder/encoder.py
+++ Encoder_Decoder/encoder.py
@@ -4,117 +4,210 @@ from Unet.unet import ResnetBlock, Downsample, Normalize, nonlinearity, Upsample
 from Unet.attention import make_attention
 import numpy as np 
 
-class Encoder(nn.Module):
+# class Encoder(nn.Module):
 
-    def __init__(self,
-                 *,
-                 ch,
-                 out_ch,
-                 ch_mult=(1, 2, 4, 8),
-                 num_res_blocks,
-                 attn_resolutions,
-                 dropout=0.0,
-                 resamp_with_conv=True,
-                 in_channels,
-                 resolution,
-                 z_channels,
-                 double_z=False,
-                 use_linear_attn=False,
-                 attn_type="vanilla",
-                 **ignore_kwargs
-                 ):
+#     def __init__(self,
+#                  *,
+#                  ch,
+#                  out_ch,
+#                  ch_mult=(1, 2, 4, 8),
+#                  num_res_blocks,
+#                  attn_resolutions,
+#                  dropout=0.0,
+#                  resamp_with_conv=True,
+#                  in_channels,
+#                  resolution,
+#                  z_channels,
+#                  double_z=False,
+#                  use_linear_attn=False,
+#                  attn_type="vanilla",
+#                  **ignore_kwargs
+#                  ):
         
 
+#         super().__init__()
+#         if use_linear_attn: attn_type = "linear"
+#         self.ch = ch 
+#         self.temb_ch = 0 
+#         self.num_resolutions = len(ch_mult)
+#         self.num_res_blocks = num_res_blocks
+#         self.resolution = resolution
+#         self.in_channels = in_channels
+
+#         # downsampling 
+#         self.conv_in = torch.nn.Conv2d(in_channels=in_channels, out_channels=self.ch, kernel_size=3, stride=1, padding=1)
+
+#         curr_res = resolution
+#         in_ch_mult = (1,)+tuple(ch_mult)
+#         self.in_ch_mult = in_ch_mult
+#         self.down = nn.ModuleList()
+        
+#         for i_level in range(self.num_resolutions):
+#             block = nn.ModuleList()
+#             attn = nn.ModuleList()
+#             block_in = ch * in_ch_mult[i_level]
+#             block_out = ch * ch_mult[i_level]
+
+#             for i_block in range(self.num_res_blocks):
+#                 block.append(ResnetBlock(in_channels=block_in, out_channels=block_out, temb_channels=self.temb_ch, dropout=dropout))
+
+#                 block_in = block_out
+#                 if curr_res in attn_resolutions:
+#                     attn.append(make_attention(in_channels=block_in, attention_type=attn_type))
+
+#             down = nn.Module()
+#             down.block = block
+#             down.attn = attn
+
+#             if i_level != self.num_resolutions -1:
+#                 down.downsample = Downsample(in_channels=block_in, with_conv=resamp_with_conv)
+#                 curr_res = curr_res // 2 
+
+#             self.down.append(down)
+
+        
+#         # middle 
+#         self.mid = nn.Module()
+#         self.mid.block_1 = ResnetBlock(in_channels=block_in, out_channels=block_in, temb_channels=self.temb_ch, dropout=dropout)
+#         self.mid.attn_1 = make_attention(in_channels=block_in, attention_type=attn_type)
+#         self.mid.block_2 = ResnetBlock(in_channels=block_in, out_channels=block_in, temb_channels=self.temb_ch, dropout=dropout)
+
+#         # ENd 
+#         self.norm_out = Normalize(block_in)
+#         self.conv_out = nn.Conv2d(in_channels=block_in, out_channels=2*z_channels if double_z else z_channels, kernel_size=3, stride=1, padding=1)
+
+
+#     def forward(self, x):
+
+#         # Timestep embedding 
+#         temb = None 
+
+#         # downsampling 
+#         hs = [self.conv_in(x)]
+#         print("shape of hs: ", hs[-1].shape)
+#         for i_level in range(self.num_resolutions):
+#             for i_block in range(self.num_res_blocks):
+
+#                 h = self.down[i_level].block[i_block](hs[-1], temb)
+#                 print("shape of h: ", h.shape)
+#                 if len(self.down[i_level].attn) > 0:
+#                     h = self.down[i_level].attn[i_block](h)
+
+#                 hs.append(h)
+
+#             if i_level != self.num_resolutions -1:
+#                 hs.append(self.down[i_level].downsample(hs[-1]))
+
+
+#         # Middle 
+#         hs = hs[-1]
+#         h = self.mid.block_1(h, temb)
+#         print("shape of block_1: ", h.shape)
+#         h = self.mid.attn_1(h)
+#         print("shape of attention_1: ", h.shape)
+#         h = self.mid.block_2(h, temb)
+#         print("shape of block_2: ", h.shape)
+
+#         # End 
+#         h = self.norm_out(h)
+#         h = nonlinearity(h)
+#         h = self.conv_out(h)
+#         print("shape of out_conv: ", h.shape)
+
+#         return h 
+
+class Encoder(nn.Module):
+    def __init__(self, *, ch, out_ch, ch_mult=(1,2,4,8), num_res_blocks,
+                 attn_resolutions, dropout=0.0, resamp_with_conv=True, in_channels,
+                 resolution, z_channels, double_z=True, use_linear_attn=False, attn_type="vanilla",
+                 **ignore_kwargs):
         super().__init__()
         if use_linear_attn: attn_type = "linear"
-        self.ch = ch 
-        self.temb_ch = 0 
+        self.ch = ch
+        self.temb_ch = 0
         self.num_resolutions = len(ch_mult)
         self.num_res_blocks = num_res_blocks
         self.resolution = resolution
         self.in_channels = in_channels
 
-        # downsampling 
-        self.conv_in = torch.nn.Conv2d(in_channels=in_channels, out_channels=self.ch, kernel_size=3, stride=1, padding=1)
+        # downsampling
+        self.conv_in = torch.nn.Conv2d(in_channels,
+                                       self.ch,
+                                       kernel_size=3,
+                                       stride=1,
+                                       padding=1)
 
         curr_res = resolution
         in_ch_mult = (1,)+tuple(ch_mult)
         self.in_ch_mult = in_ch_mult
         self.down = nn.ModuleList()
-        
         for i_level in range(self.num_resolutions):
             block = nn.ModuleList()
             attn = nn.ModuleList()
-            block_in = ch * in_ch_mult[i_level]
-            block_out = ch * ch_mult[i_level]
-
+            block_in = ch*in_ch_mult[i_level]
+            block_out = ch*ch_mult[i_level]
             for i_block in range(self.num_res_blocks):
-                block.append(ResnetBlock(in_channels=block_in, out_channels=block_out, temb_channels=self.temb_ch, dropout=dropout))
-
+                block.append(ResnetBlock(in_channels=block_in,
+                                         out_channels=block_out,
+                                         temb_channels=self.temb_ch,
+                                         dropout=dropout))
                 block_in = block_out
                 if curr_res in attn_resolutions:
-                    attn.append(make_attention(in_channels=block_in, attention_type=attn_type))
-
+                    attn.append(make_attention(block_in, attention_type=attn_type))
             down = nn.Module()
             down.block = block
             down.attn = attn
-
-            if i_level != self.num_resolutions -1:
-                down.downsample = Downsample(in_channels=block_in, with_conv=resamp_with_conv)
-                curr_res = curr_res // 2 
-
+            if i_level != self.num_resolutions-1:
+                down.downsample = Downsample(block_in, resamp_with_conv)
+                curr_res = curr_res // 2
             self.down.append(down)
 
-        
-        # middle 
+        # middle
         self.mid = nn.Module()
-        self.mid.block_1 = ResnetBlock(in_channels=block_in, out_channels=block_in, temb_channels=self.temb_ch, dropout=dropout)
-        self.mid.attn_1 = make_attention(in_channels=block_in, attention_type=attn_type)
-        self.mid.block_2 = ResnetBlock(in_channels=block_in, out_channels=block_in, temb_channels=self.temb_ch, dropout=dropout)
+        self.mid.block_1 = ResnetBlock(in_channels=block_in,
+                                       out_channels=block_in,
+                                       temb_channels=self.temb_ch,
+                                       dropout=dropout)
+        self.mid.attn_1 = make_attention(block_in, attention_type=attn_type)
+        self.mid.block_2 = ResnetBlock(in_channels=block_in,
+                                       out_channels=block_in,
+                                       temb_channels=self.temb_ch,
+                                       dropout=dropout)
 
-        # ENd 
+        # end
         self.norm_out = Normalize(block_in)
-        self.conv_out = nn.Conv2d(in_channels=block_in, out_channels=2*z_channels if double_z else z_channels, kernel_size=3, stride=1, padding=1)
-
+        self.conv_out = torch.nn.Conv2d(block_in,
+                                        2*z_channels if double_z else z_channels,
+                                        kernel_size=3,
+                                        stride=1,
+                                        padding=1)
 
     def forward(self, x):
+        # timestep embedding
+        temb = None
 
-        # Timestep embedding 
-        temb = None 
-
-        # downsampling 
+        # downsampling
         hs = [self.conv_in(x)]
-        print("shape of hs: ", hs[-1].shape)
         for i_level in range(self.num_resolutions):
             for i_block in range(self.num_res_blocks):
-
                 h = self.down[i_level].block[i_block](hs[-1], temb)
-                print("shape of h: ", h.shape)
                 if len(self.down[i_level].attn) > 0:
                     h = self.down[i_level].attn[i_block](h)
-
                 hs.append(h)
-
-            if i_level != self.num_resolutions -1:
+            if i_level != self.num_resolutions-1:
                 hs.append(self.down[i_level].downsample(hs[-1]))
 
-
-        # Middle 
-        hs = hs[-1]
+        # middle
+        h = hs[-1]
         h = self.mid.block_1(h, temb)
-        print("shape of block_1: ", h.shape)
         h = self.mid.attn_1(h)
-        print("shape of attention_1: ", h.shape)
         h = self.mid.block_2(h, temb)
-        print("shape of block_2: ", h.shape)
 
-        # End 
+        # end
         h = self.norm_out(h)
         h = nonlinearity(h)
         h = self.conv_out(h)
-        print("shape of out_conv: ", h.shape)
-
-        return h 
+        return h
     
 
 
