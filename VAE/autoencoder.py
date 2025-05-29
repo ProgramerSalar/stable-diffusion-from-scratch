25999c9b8146d16b58bee4c85e884c2b9021cf7d
ProgramerSalar <manishkumar60708090@gmail.com>
2025-05-29 18:19:38 +0530

    finally i am train the autoencoder

--- VAE/autoencoder.py
+++ VAE/autoencoder.py
@@ -6,7 +6,8 @@ import yaml
 from Distribution.distribution import DiagonalGaussianDistribution
 from Dataset.lsun import LSUNBase
 import os 
-
+import pytorch_lightning as pl 
+from torch.utils.data import DataLoader
 
 
 def instantiate_from_config(config):
@@ -25,7 +26,7 @@ def get_obj_from_str(string, reload=False):
     
 
 
-class AutoEncoderKL(nn.Module):
+class AutoEncoderKL(pl.LightningModule):
 
     
 
@@ -35,12 +36,10 @@ class AutoEncoderKL(nn.Module):
                  lossconfig=None,
                  ckpt_path=None,
                  ignore_keys=[],
-                 image_key="image",
                  colorize_nlabels=None,
                  monitor=None):
         
         super().__init__()
-        self.image_key = image_key
         self.encoder = Encoder(**ddconfig)
         self.decoder = Decoder(**ddconfig)
 
@@ -74,6 +73,14 @@ class AutoEncoderKL(nn.Module):
             pass 
 
 
+        # Add default loss 
+        if lossconfig is None:
+            self.loss = self.default_loss
+
+        else:
+            pass 
+
+
     def encode(self, x):
         # [(1, 3, 256, 256)] -> ([1, 6, 254, 254])
         h = self.encoder(x)
@@ -87,7 +94,7 @@ class AutoEncoderKL(nn.Module):
 
     def decode(self, z):
         # torch.Size([1, 3, 254, 254]) -> torch.Size([1, 3, 254, 254])
-        print("what is the shape of z: ", z.shape)
+        # print("what is the shape of z: ", z.shape)
         z = self.post_quant_conv(z)
         # torch.Size([1, 3, 254, 254]) -> torch.Size([1, 3, 254, 254])
         dec = self.decoder(z)
@@ -112,12 +119,12 @@ class AutoEncoderKL(nn.Module):
             z = posterior.mode()
             z = z.half()
 
-        # torch.Size([1, 3, 254, 254]) -> torch.Size([1, 3, 1016, 1016])
+        
         dec = self.decode(z)
-        print("what is shape of decoder: ", dec)
+        # print("what is shape of decoder: ", dec)
         
 
-        # torch.Size([1, 3, 1016, 1016]), ([1, 6, 254, 254])
+        
         return dec, posterior
     
 
@@ -131,30 +138,127 @@ class AutoEncoderKL(nn.Module):
 
         return batch 
     
+    def get_last_layer(self):
+        return self.decoder.conv_out.weight
     
-    def vae_loss(recon_x, 
-                 x,
-                 posterior):
+    
+    def training_step(self, batch, batch_idx):
+        inputs = self.get_input(batch)
+        reconstructions, posterior = self(inputs)
+
+
+        # train encoder+decoder+logvar 
+        aeloss, log_dict_ae = self.loss(inputs,
+                                        reconstructions=reconstructions,
+                                        posterior=posterior,
+                                        global_step=self.global_step,
+                                        last_layer=self.get_last_layer(),
+                                        split="train")
         
-        recon_loss = nn.functional.mse_loss(recon_x, x)
-        kl_loss = posterior.kl().mean()
+        self.log("aeloss", aeloss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        self.log_dict(log_dict_ae, prog_bar=False, logger=True, on_step=True, on_epoch=False)
 
-        return recon_loss +  kl_loss
+        return aeloss
     
 
+    def validation_step(self, batch, batch_idx):
+        inputs = self.get_input(batch)
+        reconstructions, posterior = self(inputs)
 
+        aeloss, log_dict_ae = self.loss(inputs,
+                                        reconstructions=reconstructions,
+                                        posterior=posterior,
+                                        global_step=self.global_step,
+                                        last_layer=self.get_last_layer(),
+                                        split="val")
+        
+        self.log("val/rec_loss", log_dict_ae["val/rec_loss"])
+        self.log_dict(log_dict_ae)
+        
+        return self.log_dict
+    
 
     def configure_optimizers(self):
-        lr = self.learning_rate 
-        opt_ae = torch.optim.Adam(list(self.encoder.parameters()) + 
+        lr = self.learning_rate
+        opt_ae = torch.optim.Adam(list(self.encoder.parameters()) +
                                   list(self.decoder.parameters()) + 
-                                  list(self.quant_conv.parameters())+
+                                  list(self.quant_conv.parameters()) + 
                                   list(self.post_quant_conv.parameters()),
                                   lr=lr,
-                                  betas=(0.5, 0.9))
+                                  betas=(0.5, 0.9)
+                                  )
         
 
-        return [opt_ae]
+        return [opt_ae], []
+    
+
+    def default_loss(self, 
+                     inputs,
+                     reconstructions, 
+                     posterior, 
+                     global_step,
+                     last_layer,
+                     split
+                     ):
+        
+        # Reconstruction loss (MSE)
+        rec_loss = nn.functional.mse_loss(inputs, reconstructions)
+
+        # KL Divergance loss 
+        kl_loss = posterior.kl().mean()
+        total_loss = rec_loss + 0.5 * kl_loss
+
+        # Logging 
+        log_dict = {
+            f"{split}/rec_loss": rec_loss.detach(),
+            f"{split}/kl_loss": kl_loss.detach(),
+            f"{split}/total_loss": total_loss.detach()
+        }
+
+        return total_loss, log_dict
+    
+
+    
+
+
+    # @torch.no_grad()
+    # def log_images(self, batch, only_inputs=False, **kwargs):
+    #     log = dict()
+    #     x = self.get_input(batch).cuda()
+
+    #     if not only_inputs:
+    #         xrec, posterior = self(x)
+    #         if x.shape[1] > 3:
+    #             assert xrec.shape[1] > 3 
+    #             x = self.to_rgb(x)
+    #             xrec = self.to_rgb(xrec)
+
+
+    #         log["samples"] = self.decode(torch.randn_like(posterior.sample()))
+    #         log["reconstructions"] = xrec
+
+    #     log["inputs"] = x 
+    #     return log 
+    
+
+
+
+
+    # def to_rgb(self, x):
+    #     if not hasattr(self, "colorize"):
+    #         self.register_buffer("colorize", torch.randn(3, x.shape[1], 1, 1).to(x))
+
+    #     x = nn.functional.conv2d(x, weight=self.colorize)
+    #     x = 2.*(x-x.min())/(x.max()-x.min())-1.
+
+    #     return x 
+
+
+
+
+
+
+
     
 
 
@@ -171,15 +275,48 @@ class AutoEncoderKL(nn.Module):
 
 if __name__ == "__main__":
 
-    # Enable expandable segments to reduce fragmentation
-    os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
-    torch.cuda.empty_cache()
+    # # # Enable expandable segments to reduce fragmentation
+    # os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
+    # torch.cuda.empty_cache()
 
-    # Use FP16 if possible
-    torch.backends.cudnn.benchmark = True
+    # # Use FP16 if possible
+    # torch.backends.cudnn.benchmark = True
 
-    x = torch.randn(1, 3, 128, 128).half().cuda()
+    # x = torch.randn(1, 3, 256, 256).half().cuda()
 
+
+    # config = "config/config.yaml"
+
+    # # Load the YAML file 
+    # with open(config, 'r') as file:
+    #     config = yaml.safe_load(file)
+
+    
+    # autoencoder = AutoEncoderKL(ddconfig=config['model']['params']['ddconfig'],
+    #                             embed_dim=config['model']['embed_dim']).to("cuda").half().cuda()
+    
+    # # print(autoencoder)
+    # recon_x, posterior = autoencoder(x)
+    # print("reconstructed shape of data: ", recon_x.shape)
+    # print("posterior: ", posterior)
+
+    
+    # # Run with mixed precision 
+    # with torch.amp.autocast('cuda'):
+    #     output = autoencoder(x)
+    #     output = output[0]
+        
+    # print("Tensor of output: ", output.shape)
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+
+    x = torch.randn(4, 3, 256, 256).half().cuda()
+    y = torch.randn(4, 3, 256, 256).half().cuda()
+
+
+    train_data = DataLoader(x, batch_size=4)
+    test_data = DataLoader(y, batch_size=4)
+    # print(train_data)
 
     config = "config/config.yaml"
 
@@ -189,33 +326,32 @@ if __name__ == "__main__":
 
     
     autoencoder = AutoEncoderKL(ddconfig=config['model']['params']['ddconfig'],
-                                embed_dim=config['model']['embed_dim']).to("cuda").half().cuda()
+                                embed_dim=config['model']['embed_dim'],
+                                monitor="val/total_loss"
+                                ).half().cuda()
     
-    # print(autoencoder)
-    recon_x, posterior = autoencoder(x)
-    print("reconstructed shape of data: ", recon_x.shape)
-    print("posterior: ", posterior)
+
 
     
-    # Run with mixed precision 
-    with torch.amp.autocast('cuda'):
-        output = autoencoder(x)
-        
-    print("Tensor of output: ", output)
 
-        # Training loop 
-        # for epoch in range(10):
-        #     recon_x, posterior = autoencoder(x)
-        #     loss = AutoEncoderKL.vae_loss(recon_x=recon_x,
-        #                                   x=x,
-        #                                   posterior=posterior)
-        #     optimizer = AutoEncoderKL.configure_optimizers()
-        #     optimizer.zero_grad()
-        #     loss.backward()
-        #     optimizer.step()
-        #     print(f"Loss: {loss.item()}")
+    trainer = pl.Trainer(
+        max_epochs=10,
+        devices=1, 
+        accelerator="gpu"
+        # enable_progress_bar=True,
+        # progress_bar_refresh_rate=1,
+        # checkpoint_callback=True
+    )
 
-    
+
+    trainer.fit(
+        autoencoder,
+        train_dataloaders=train_data,
+        val_dataloaders=test_data
+    )
+
+
+
     
 
 
